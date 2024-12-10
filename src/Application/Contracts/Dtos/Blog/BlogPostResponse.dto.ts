d202cee2b6e5b03b5559b0a81e6e673aa30ad83c
cloudjohnsp <jvavelino2017@gmail.com>
2024-12-10 16:49:57 -0300

    test: renames file

--- src/Application/Contracts/Dtos/Blog/BlogPostResponse.dto.ts
+++ src/Application/Contracts/Dtos/Blog/BlogPostResponse.dto.ts
@@ -1,12 +1,22 @@
 import { Types } from 'mongoose';
 
 interface BlogPostResponse {
-  id: Types.ObjectId;
-  title: string;
-  slug: string;
-  content: string;
-  tags: string[];
-  createdAt: Date;
+  id?: Types.ObjectId;
+  title?: string;
+  slug?: string;
+  content?: string;
+  tags?: string[];
+  createdAt?: Date;
+  updatedAt?: Date;
+  upVotes?: number;
+  downVotes?: number;
+  comments?: [
+    {
+      user: string;
+      content: string;
+      votes: number;
+    }
+  ];
 }
 
 export default BlogPostResponse;
--- src/Application/UseCases/Blog/FindByTitleUseCase.ts
+++ src/Application/UseCases/Blog/FindByTitleUseCase.ts
@@ -14,6 +14,7 @@ class FindByTitleUseCase {
         content: item.content,
         tags: item.tags,
         createdAt: item.createdAt,
+        updatedAt: item.updatedAt,
       };
     });
 
